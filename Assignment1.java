cd8616de1c8efd841b3913f71b473621fc2857cb
chaitracn248 <chaitracn8@gmail.com>
2026-03-20 21:44:31 +0530

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,16 +1,57 @@
-class Assignment1 {
-  public static int[] findLeapYears(int year) {
-  // Implement your code here and change the return value accordingly
-    
-  return null;
-  }
-  
-  public static void main(String[] args) {
-    int year = 2000;
-    int[] leapYears;
-    leapYears = findLeapYears(year);
-    for (int index = 0; index < leapYears.length; index++) {
-      System.out.println(leapYears[index]);
-    }
-  }
+class IntStack {
+        private int top;
+        private int[] arr;
+
+        IntStack(int size) {
+                top = -1;
+                arr = new int[size];
+        }
+
+        public boolean isEmpty() {
+                return top == -1;
+        }
+
+        public void push(int data) {
+                arr[++top] = data;
+        }
+
+        public int pop() {
+                return arr[top--];
+        }
+}
+
+class Tester {
+        public static int calculateTotal(String input) {
+
+                IntStack stack = new IntStack(10);
+                int num = 0;
+
+                for(int i = 0; i < input.length(); i++) {
+                        char ch = input.charAt(i);
+
+                        if(Character.isDigit(ch)) {
+                                num = num * 10 + (ch - '0');
+                        }
+                        else if(ch == ',') {
+                                stack.push(num);
+                                num = 0;
+                        }
+                }
+
+                // Push last number
+                stack.push(num);
+
+                int total = 0;
+                while(!stack.isEmpty()) {
+                        total += stack.pop();
+                }
+
+                return total;
+        }
+
+        public static void main(String[] args) {
+                String input = "Rice-50,Oil-120,Sugar-40";
+                int total = calculateTotal(input);
+                System.out.println("Total Bill: " + total);
+        }
 }
